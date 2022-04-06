8f6dc8b36a5e824c64299d206d67b55c6cdf9398
imchino <chino618@me.com>
2022-04-07 00:25:12 +0900

    Initial Commit

---
+++ Seismometer/SeismometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeismometerApp.swift
+//  Seismometer
+//
+//  Created by chino on 2022/04/04.
+//
+
+import SwiftUI
+
+@main
+struct SeismometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SeismometerBrowser()
+        }
+    }
+}
